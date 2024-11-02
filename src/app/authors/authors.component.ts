dc3316bcc13c462dbbb89aeedd86549ebad4afd1
angue047 <angue047@uottawa.ca>
2024-11-02 16:47:28 -0400

    more author update

--- src/app/authors/authors.component.ts
+++ src/app/authors/authors.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
+import {ActivatedRoute, Router, RouterOutlet, Routes} from "@angular/router";
+import {AuthorComponent} from "./author/author.component";
+
+export const booksRoutes: Routes = [
+  {path: ':id', component: AuthorComponent}
+];
 
 @Component({
   selector: 'app-authors',
-  standalone: true,
-  imports: [],
   templateUrl: './authors.component.html',
-  styleUrl: './authors.component.css'
+  styleUrl: './authors.component.css',
+  standalone: true,
+  imports: [RouterOutlet]
 })
-export class AuthorsComponent {
 
+export class AuthorsComponent {
+  constructor(private router: Router, private route: ActivatedRoute) { }
+
+  submit(value: string): void {
+    this.router.navigate(['./', value], {relativeTo: this.route}).then(r => {});
+  }
 }
